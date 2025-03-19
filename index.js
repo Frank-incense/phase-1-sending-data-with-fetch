bb733d5e219de4e020736f5d3fe9d12ac1bafc19
Frankincense <frankincensewelsey@gmail.com>
2025-03-19 20:36:54 +0300

    using fetch to post to mock server

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const userData = {
+        name: userName,
+        email: userEmail
+    } ;
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    }).then(res => res.json())
+    .then(user=>{
+        const body = document.querySelector('body')
+        const p = document.createElement('p')
+        p.textContent=user.id
+        body.appendChild(p)
+    }).catch((err)=>{
+        const body = document.querySelector('body')
+        const p = document.createElement('p')
+        p.textContent= err.message
+        body.appendChild(p)
+    })
+}
+
+submitData("frank", "frank@example.com")
